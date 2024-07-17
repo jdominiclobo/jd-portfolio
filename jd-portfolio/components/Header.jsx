409b1cdf08fd98cf6bd2b09fd8519a58fed8f41a
Joel Dominic Lobo <joeldominiclobo@Joels-MacBook-Air.local>
2024-07-17 14:03:08 +0530

    setup:initial tailwind setup

---
+++ jd-portfolio/components/Header.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">Header</div>
+    </header>
+  );
+};
+
+export default Header;
